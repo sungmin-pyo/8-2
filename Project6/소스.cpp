f724179a66079fdf4464bf338002f9d11647da68
성민 표 <pmm7812@naver.com>
2019-11-22 15:15:46 +0900

    초기 커밋.

---
+++ Project6/소스.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	int data1 = 10;
+	int data2 = 20;
+	int sum = 0;
+
+	int* p = &data1;
+	*p = 100;
+	sum += *p;
+
+	p = &data2;
+	*p = 200;
+	sum += *p;
+
+	printf("sumÀÇ °ª = %d\n", sum);
+
+	return 0;
+}
